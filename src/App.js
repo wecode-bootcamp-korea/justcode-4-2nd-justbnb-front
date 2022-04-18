c8bfda49f57985b89e710f058b3f776394bea42b
minji Kong <minzyaaaaaa@gmail.com>
2022-04-18 21:18:19 +0900

    Add : Front 초기 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import Detail from './pages/Detail/Detail';
+import List from './pages/List/List';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
