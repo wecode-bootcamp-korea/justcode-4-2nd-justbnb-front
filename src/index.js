251d781b4154a80314649f367708c0e3abe243b8
💗 쓸💗 <sseul@ui-MacBookAir.local>
2022-04-18 19:16:11 +0900

    Add : front 초기 세팅

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GlobalStyle from './styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+import theme from './styles/theme';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </>
+);
