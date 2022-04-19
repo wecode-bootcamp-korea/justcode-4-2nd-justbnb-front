5603c17b7f78d4ae36af318a55453acc94de0fba
💗 쓸💗 <sseul@ui-MacBookAir.local>
2022-04-19 11:57:00 +0900

    ADD: 추가 초기세팅

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Detail from './pages/Detail/Detail';
 import List from './pages/List/List';
+import Hosting from './pages/Hosting/Hosting';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/list" element={<List />} />
+        <Route path="/hosting" element={<Hosting />} />
       </Routes>
     </BrowserRouter>
   );
