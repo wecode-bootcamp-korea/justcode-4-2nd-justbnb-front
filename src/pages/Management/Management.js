ccba9e2cfebb2ce163f9edc51a512fbf081a1651
dlwjdals <jml22zzang@gmail.com>
2022-04-19 21:47:16 +0900

    Add : management - 숙소관리 페이지 구현중

--- src/pages/Management/Management.js
+++ src/pages/Management/Management.js
@@ -2,6 +2,14 @@ import { useState, useEffect } from 'react';
 import style from 'styled-components';
 
 function Management() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/hwseol/list.json')
+      .then(res => res.json())
+      .then(data => setData(data));
+  }, []);
+
   const Banner = style.div`
   height : 10em;
   margin : 1em 0 2em 0;
@@ -15,7 +23,6 @@ function Management() {
   const BookingText = style.div`
   font-size : 30px;
   font-weight : bold;
-  
   `;
 
   const AllBooking = style.div`
@@ -28,7 +35,7 @@ function Management() {
   const ManageButtons = style.div`
   display : flex;
   flex-flow = wrap;
-  margin : 50px 0 30px 0;
+  margin : 10px 0 30px 0;
   `;
 
   const Button = style.div`
@@ -39,13 +46,53 @@ function Management() {
   `;
 
   const Accommodation = style.div`
-  height: 250px;
+  height: content-fit;
   background-color : rgba(0,0,0,0.05);
-  border-radius : 13px;
+  display : flex;
+  justify-content : space-between;
+  border-radius : 30px;
+  padding : 30px;
   `;
 
   const ManagementFooter = style.div`
   background-color : black;
+  margin-top: 100px;
+  padding-bottom : 0px;
+
+  `;
+
+  const Text = style.div`
+  color : white;
+  font-size : 30px;
+  font-weight : bold;
+  margin-left : 3.7em;
+  padding : 60px 0 20px 0;
+  `;
+
+  const Tips = style.div`
+  display : flex;
+  justify-content : space-between;
+  margin : 0 7em;
+  padding : 20px 0px 60px 0px;
+  `;
+
+  const Card = style.div`
+  display : flex;
+  flex-direction : column;
+  border-radius : 20px;
+  overflow : hidden;
+  `;
+
+  const CardDescription = style.div`
+  color : white;
+  padding: 30px 0px 50px 10px;
+  background-color : rgba(100, 100, 100, 0.6);
+  font-size : 20px;`;
+
+  const Img = style.img`
+  width : 340px;
+  height : auto;
+  
   `;
 
   return (
@@ -60,10 +107,41 @@ function Management() {
           <Button>체크아웃 예정(0건)</Button>
           <Button>예정(0건)</Button>
         </ManageButtons>
-        <Accommodation>&nbsp;</Accommodation>
+        <Accommodation>
+          <Card>
+            <Img src={data[5].image} />
+            <CardDescription>{data[5].name}</CardDescription>
+          </Card>
+          <Card>
+            <Img src={data[5].image} />
+            <CardDescription>{data[5].name}</CardDescription>
+          </Card>
+          <Card>
+            <Img src={data[5].image} />
+            <CardDescription>{data[5].name}</CardDescription>
+          </Card>
+        </Accommodation>
       </GuestCardWide>
       <ManagementFooter>
-        
+        <Text>호스팅 관련 팁과 업데이트</Text>
+        <Tips>
+          <Card>
+            <Img src={data[1].image} />
+            <CardDescription>{data[1].name}</CardDescription>
+          </Card>
+          <Card>
+            <Img src={data[2].image} />
+            <CardDescription>{data[2].name}</CardDescription>
+          </Card>
+          <Card>
+            <Img src={data[3].image} />
+            <CardDescription>{data[3].name}</CardDescription>
+          </Card>
+          <Card>
+            <Img src={data[5].image} />
+            <CardDescription>{data[5].name}</CardDescription>
+          </Card>
+        </Tips>
       </ManagementFooter>
     </div>
   );
