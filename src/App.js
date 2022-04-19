0783452e056e18752d102bfeb7e2193d3e3bb84d
shw779 <shw779@naver.com>
2022-04-19 12:14:16 +0900

    [FIX]기존 list 제거

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AccommodationList from './pages/List/AccommodationList';
 import Main from './pages/Main/Main';
 import Detail from './pages/Detail/Detail';
-import List from './pages/List/List';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Route path="/list/:localName" element={<AccommodationList />} />
         <Route path="/" element={<Main />} />
         <Route path="/detail" element={<Detail />} />
-        <Route path="/list" element={<List />} />
       </Routes>
     </BrowserRouter>
   );
