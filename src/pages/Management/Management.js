29ce677eb286149f5b7a08b32f5ac66b84d8be6e
dlwjdals <jml22zzang@gmail.com>
2022-04-20 22:18:11 +0900

    Fix : management - 슬라이드 구현 중...

--- src/pages/Management/Management.js
+++ src/pages/Management/Management.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import style from 'styled-components';
+import styled from 'styled-components';
 
 function Management() {
   const [data, setData] = useState([]);
@@ -67,23 +67,26 @@ function Management() {
           })}
           {/* 여기까지 슬라이드 */}
         </Accommodation>
+        <button>왼쪽</button>
+        <button>오른쪽</button>
       </GuestCardWide>
-      <button>왼쪽</button>
-      <button>오른쪽</button>
+
       <ManagementFooter>
-        <Text>호스팅 관련 팁과 업데이트</Text>
-        <Tips>
-          {data.map((el, index) => {
-            if (index <= 3) {
-              return (
-                <Card key={el.id}>
-                  <Img src={el.image} alt="test" />
-                  <CardDescription>{el.name}</CardDescription>
-                </Card>
-              );
-            }
-          })}
-        </Tips>
+        <TextAndTips>
+          <Text>호스팅 관련 팁과 업데이트</Text>
+          <Tips>
+            {data.map((el, index) => {
+              if (index <= 3) {
+                return (
+                  <Card key={el.id}>
+                    <Img src={el.image} alt="test" />
+                    <CardDescription>{el.name}</CardDescription>
+                  </Card>
+                );
+              }
+            })}
+          </Tips>
+        </TextAndTips>
       </ManagementFooter>
     </div>
   );
@@ -91,110 +94,145 @@ function Management() {
 
 export default Management;
 
-const Banner = style.div`
-height : 10em;
-margin : 1em 0 2em 0;
-background: rgb(27,0,209);
-background: linear-gradient(90deg, rgba(27,0,209,1) 0%, rgba(255,0,168,1) 100%);
+const Banner = styled.div`
+  height: 10em;
+  margin: 1em 0 2em 0;
+  background: rgb(27, 0, 209);
+  background: linear-gradient(
+    90deg,
+    rgba(27, 0, 209, 1) 0%,
+    rgba(255, 0, 168, 1) 100%
+  );
 `;
 
-const GuestCardWide = style.div`
-margin : 0 7em;
+const GuestCardWide = styled.div`
+  margin: 0 7em;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  border: 1px solid red;
+  /* max-width: 100%; */
 `;
 
-const BookingText = style.div`
-font-size : 30px;
-font-weight : bold;
+const BookingText = styled.div`
+  font-size: 30px;
+  font-weight: bold;
 `;
 
-const AllBooking = style.div`
-font-size : 15px;
-font-weight : bold;
-text-align : right;
-text-decoration: underline;
+const AllBooking = styled.div`
+  font-size: 15px;
+  font-weight: bold;
+  text-align: right;
+  text-decoration: underline;
 `;
 
-const ManageButtons = style.div`
-display : flex;
-flex-flow = wrap;
-margin : 10px 0 30px 0;
+const ManageButtons = styled.div`
+  display: flex;
+  /* flex-flow = wrap; */
+  margin: 10px 0 30px 0;
 `;
 
-const Button = style.div`
-border-radius : 50px;
-border : 1px solid rgba(0,0,0,0.4);
-margin-right : 10px;
-padding : 15px;
+const Button = styled.div`
+  border-radius: 50px;
+  border: 1px solid rgba(0, 0, 0, 0.4);
+  margin-right: 10px;
+  padding: 15px;
 `;
-const ManagementFooter = style.div`
-background-color : black;
-margin-top: 100px;
-padding-bottom : 0px;
-
-`;
-
-const Text = style.div`
-color : white;
-font-size : 30px;
-font-weight : bold;
-margin-left : 3.7em;
-padding : 60px 0 20px 0;
+const ManagementFooter = styled.div`
+  display: flex;
+  justify-content: center;
+  /* flex-direction: column;
+  align-items: center; */
+  background-color: black;
+  margin-top: 100px;
+  padding-bottom: 0px;
 `;
 
-const Tips = style.div`
-display : flex;
-justify-content : space-between;
-margin : 0 7em;
-padding : 20px 0px 60px 0px;
+const TextAndTips = styled.div`
+  display: flex;
+  margin: 0 7em;
+  justify-content: flex-start;
+  flex-direction: column;
+  /* align-items: center; */
+  /* width: 50%; */
+  /* border: 1px solid red; */
 `;
 
-const Card = style.div`
-display : flex;
-flex-direction : column;
-border-radius : 20px;
-margin : 0 10px;
-overflow : hidden;
+const Text = styled.div`
+  color: white;
+  text-align: left;
+  font-size: 30px;
+  font-weight: bold;
+  margin-left: 3.7em;
+  padding: 60px 0 20px 0;
+  /* border: 1px solid red; */
 `;
 
-const CardDescription = style.div`
-color : white;
-padding: 30px 0px 50px 10px;
-background-color : rgba(100, 100, 100, 0.6);
-font-size : 20px;`;
+const Tips = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: fit-content;
+  margin: 0 7em;
+  padding: 20px 0px 60px 0px;
+`;
 
-const Img = style.img`
-min-width : 340px;
-height : auto;
-min-height : 15em;
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 20px;
+  margin: 0 10px;
+  overflow: hidden;
+`;
+
+const CardDescription = styled.div`
+  color: white;
+  padding: 30px 0px 50px 10px;
+  background-color: rgba(100, 100, 100, 0.6);
+  font-size: 20px;
+`;
+
+const Img = styled.img`
+  min-width: 340px;
+  height: 17em;
+  /* min-height: 17em; */
 `;
 //-----------------------------------------------------------------------
-const Accommodation = style.div`
-background-color : rgba(0,0,0,0.1);
-border-radius : 30px;
-display : flex;
-flex-direction : row;
-padding : 30px;
-overflow : hidden;
+const Accommodation = styled.div`
+  background-color: rgba(0, 0, 0, 0.1);
+  border-radius: 30px;
+
+  display: flex;
+  justify-content: space-between;
+  /*align-items: center; */
+  /* justify-content: space-around; */
+  /* flex-direction: row; */
+  /* display: grid;
+  grid-template-columns: repeat(800, 1fr);
+  flex-wrap: nowrap; */
+  padding: 30px;
+  overflow: hidden;
 `;
 
-const Card2 = style.div`
-display : flex;
-// border : 1px solid red;
-min-width : 300px;
-flex-direction : column;
-border-radius : 20px;
-margin : 0 10px;
-overflow : hidden;
+const Card2 = styled.div`
+  min-width: 300px;
+  border-radius: 20px;
+  margin: 0 2%;
+  overflow: hidden;
+  background-color: rgba(100, 100, 100, 1);
+  padding-bottom: 10px;
+
+  /* border : 1px solid red; */
 `;
 
-const CardDescription2 = style.div`
-color : white;
-padding: 5px 0px 5px 10px;
-background-color : rgba(100, 100, 100, 0.6);
-font-size : 15px;`;
+const CardDescription2 = styled.div`
+  color: white;
+  padding: 8px 0px 5px 10px;
+  background-color: rgba(100, 100, 100, 1);
+  font-size: 15px;
+`;
 
-const Img2 = style.img`
-min-width : 340px;
-height : auto;
-min-height : 16em;
+const Img2 = styled.img`
+  min-width: 340px;
+  height: 16em;
+  /* min-height : 16em; */
 `;
