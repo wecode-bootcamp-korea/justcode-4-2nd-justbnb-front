a8075a136c81bdbad7081d9ae1eb778e1331a043
dlwjdals <jml22zzang@gmail.com>
2022-04-23 19:36:30 +0900

    Add : hosting 페이지 - 5단계 구현 진행 중

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AccommodationList from './pages/List/AccommodationList';
 import Main from './pages/Main/Main';
 import Detail from './pages/Detail/Detail';
 import Hosting from './pages/Hosting/Hosting';
+import Hosting2 from './pages/Hosting/Hosting2';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/hosting" element={<Hosting />} />
+        <Route path="/hosting/2" element={<Hosting2 />} />
       </Routes>
     </BrowserRouter>
   );
