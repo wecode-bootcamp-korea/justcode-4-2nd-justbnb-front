8d1e4c3a2eeb68ffeffe463a48778b29cd9b09b2
dlwjdals <jml22zzang@naver.com>
2022-04-24 19:16:14 +0900

    Fix : hosting - Step 5 레이아웃 일부 수정

--- src/pages/Hosting/Hosting5.js
+++ src/pages/Hosting/Hosting5.js
@@ -136,9 +136,6 @@ const Container2 = styled.section`
 `;
 
 const Header = styled.section`
-  /* position: absolute;
-  top: 0; */
-  /* border: 1px solid red; */
   width: 100%;
   padding: 20px;
   display: flex;
@@ -168,18 +165,25 @@ const Body = styled.section`
   width: 100%;
   margin: 0 auto;
   display: flex;
+  padding: auto;
+  justify-content: center;
   flex-direction: column;
   align-items: center;
 `;
 
 const Text2 = styled.div`
-  font-size: 35px;
+  font-size: 22px;
   font-weight: bold;
   margin-right: auto;
+  margin-left: 14px;
 `;
 
 const TextAndConvenience = styled.div`
   display: flex;
+  width: fit-content;
+  /* height: fit-content; */
+  margin: auto;
+  /* padding: auto; */
   flex-direction: column;
   align-items: center;
 `;
@@ -230,11 +234,10 @@ const TextAndIcon = styled.div`
   justify-content: center;
   flex-direction: column;
   align-items: center;
-  width: 250px;
-  height: fit-content;
-  font-size: 30px;
-  font-weight: bolder;
-  padding: 40px;
+  /* width: 120% !important; */
+  width: 14vw;
+  /* height: fit-content; */
+  padding: 3vw;
   border: 1px solid rgba(0, 0, 0, 0.2);
   border-radius: 25px;
   margin: 15px;
