0d6f7dc46c22b0b7204a5ab944929281e7d8ee0f
minji Kong <minzyaaaaaa@gmail.com>
2022-04-29 12:51:27 +0900

    Fix : Reservation Card CSS 수정

--- src/pages/Reservation/Reservation.js
+++ src/pages/Reservation/Reservation.js
@@ -33,7 +33,6 @@ function Reservation() {
     <>
       <PageNav />
       <Banner>
-        <Wrap>{/* <Text2>투데이</Text2> */}</Wrap>
         <Desc>예약 내역을 확인해보세요! </Desc>
       </Banner>
       <Body>
@@ -42,20 +41,36 @@ function Reservation() {
         {/* 여기서부터 예약된 여행 있는 경우 */}
         <ReservationList>
           {data2.map((el, index) => {
-            console.log(data2);
             return (
               <Card2 key={el.id}>
                 <Img2 src={el.image_url} alt="img" />
-                <CardDescription2>
-                  예약자 이름 : {el.user_name}
-                </CardDescription2>
-                <CardDescription2>Check-In : {el.check_in}</CardDescription2>
-                <CardDescription2>Check-Out : {el.check_out}</CardDescription2>
-                <CardDescription2>총 인원 : {el.members} 명</CardDescription2>
-                <CardDescription2>숙소 이름 : {el.name}</CardDescription2>
-                <CardDescription2>
-                  총 가격 : {el.total_charge}원
-                </CardDescription2>
+                <TextWrapper>
+                  <Text>
+                    <div>예약자 이름 :</div>
+                    <div>{el.user_name}</div>
+                  </Text>
+                  <Text>
+                    <div>Check-In :</div>
+                    <div>{el.check_in}</div>
+                  </Text>
+                  <Text>
+                    <div>Check-Out :</div>
+                    <div>{el.check_out}</div>
+                  </Text>
+
+                  <Text>
+                    <div>총 인원 :</div>
+                    <div>{el.members}명</div>
+                  </Text>
+                  <Text>
+                    <div>총 가격 :</div>
+                    <div>{el.total_charge}원</div>
+                  </Text>
+                  <TextName>
+                    <div>숙소 이름 :</div>
+                    <div>{el.name}</div>
+                  </TextName>
+                </TextWrapper>
               </Card2>
             );
           })}
@@ -63,12 +78,12 @@ function Reservation() {
         {/* 여기까지 예약된 여행 있는 경우 */}
         {/* 여기서부터 예약된 여행 없는 경우 */}
         {/* <NoReservation>
-        <Text1>아직 예약된 여행이 없습니다!</Text1>
-        <Text2>
-          여행 가방에 쌓인 먼지를 털어내고 다음 여행 계획을 세워보세요.
-        </Text2>
-        <Text3 onClick={goToMain}>숙소 검색하기</Text3>
-      </NoReservation> */}
+          <Text1>아직 예약된 여행이 없습니다!</Text1>
+          <Text2>
+            여행 가방에 쌓인 먼지를 털어내고 다음 여행 계획을 세워보세요.
+          </Text2>
+          <Text3 onClick={goToMain}>숙소 검색하기</Text3>
+        </NoReservation> */}
         {/* 여기까지 예약된 여행 없는 경우 */}
         <Line />
         <Text4>예약 내역을 찾으실 수 없나요?&nbsp;</Text4>
@@ -82,26 +97,29 @@ function Reservation() {
 export default Reservation;
 
 const Banner = styled.div`
-  padding: 7rem;
-  background: rgb(27, 0, 209);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 300px;
+  padding-top: 100px;
+
+  /* background: rgb(27, 0, 209); */
   background: linear-gradient(
     90deg,
     rgba(27, 0, 209, 1) 0%,
     rgba(255, 0, 168, 1) 100%
   );
 `;
+
 const Desc = styled.div`
-  padding-left: 150px;
-  font-size: 2vw;
+  width: 100%;
+  font-size: 30px;
+  font-weight: 500;
   color: #ffffff;
+  padding-left: 80px;
 `;
 
-const Wrap = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 40px 150px;
-`;
 const Travel = styled.div`
   font-size: 40px;
   font-weight: bold;
@@ -117,15 +135,14 @@ const Body = styled.div`
 `;
 
 const ReservationList = styled.div`
-  margin: 10px 0;
   display: flex;
+  justify-content: flex-start;
 `;
 
 const Text3 = styled.div`
   cursor: pointer;
   font-size: 20px;
   font-weight: bold;
-  border: 1px solid black;
   border-radius: 10px;
   width: fit-content;
   padding: 20px 30px;
@@ -133,27 +150,83 @@ const Text3 = styled.div`
 `;
 
 const Text4 = styled.span`
-  font-size: 20px;
-  margin: 25px 0;
+  font-size: 15px;
 `;
 
 const Text5 = styled.span`
   text-decoration: underline;
-  font-size: 20px;
+  font-size: 18px;
   font-weight: bold;
 `;
 
 const Card2 = styled.div`
-  /* min-width: 300px;
-  max-width: 300px; */
-  width: 24vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 260px;
+  height: 400px;
+
   border-radius: 20px;
-  margin: 0 2.8%;
+  margin: 0 20px;
   overflow: hidden;
-  background-color: rgba(100, 100, 100, 1);
-  padding-bottom: 10px;
+  background-color: white;
+  box-shadow: 0 0 5px 5px rgba(0, 0, 0, 0.15);
 `;
 
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  height: 45%;
+  padding: 20px;
+`;
+
+const Text = styled.div`
+  display: flex;
+  justify-content: space-between;
+  div:first-child {
+    display: flex;
+    align-items: flex-start;
+    /* width: 30%; */
+    height: 100%;
+    white-space: nowrap;
+    font-weight: 600;
+    margin-right: 10px;
+    margin-bottom: 6px;
+    color: rgba(0, 0, 0, 0.8);
+    font-size: 14px;
+  }
+  div:last-child {
+    /* width: 60%; */
+    height: 100%;
+    text-align: left;
+    font-size: 14px;
+  }
+`;
+
+const TextName = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 100%;
+  div:first-child {
+    display: flex;
+    align-items: flex-start;
+    /* width: 30%; */
+    height: 100%;
+    white-space: nowrap;
+    font-weight: 600;
+    margin-right: 10px;
+    margin-bottom: 6px;
+    color: rgba(0, 0, 0, 0.8);
+    font-size: 14px;
+  }
+  div:last-child {
+    height: 100%;
+    text-align: left;
+    font-size: 14px;
+  }
+`;
 const CardDescription2 = styled.div`
   color: white;
   padding: 8px 0px 5px 10px;
@@ -164,5 +237,5 @@ const CardDescription2 = styled.div`
 const Img2 = styled.img`
   object-fit: cover;
   width: 100%;
-  height: 18vw;
+  height: 55%;
 `;
